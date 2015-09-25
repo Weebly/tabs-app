d5f65000c38034096648d2da2bc2995beee7c1e6
Matt Porter <mporter@weebly.com>
2015-09-25 16:11:36 -0700

    updated syntax styling to better match weebly standards.

--- files/html/element.tpl
+++ files/html/element.tpl
@@ -1,18 +1,18 @@
 <div class="tabbed-box {{theme}}">
-	<ul class="tabbed-box-tab-group">
-	{{#tabs_each}}
-		<li class="tabbed-box-tab" rel="tab-{{tabs_index}}">
-			{header_{{tabs_index}}:text default="Headline {{tabs_index}}"}
-		</li>
-	{{/tabs_each}}
-	</ul>
+    <ul class="tabbed-box-tab-group">
+    {{#tabs_each}}
+        <li class="tabbed-box-tab" rel="tab-{{tabs_index}}">
+            {header_{{tabs_index}}:text default="Headline {{tabs_index}}"}
+        </li>
+    {{/tabs_each}}
+    </ul>
 
-	<div class="tabbed-box-content-group">
-	{{#tabs_each}}
-		<div class="tabbed-box-content tab-{{tabs_index}}">
-			{content_{{tabs_index}}:content}
-		</div>
-	{{/tabs_each}}
-	</div>
+    <div class="tabbed-box-content-group">
+    {{#tabs_each}}
+        <div class="tabbed-box-content tab-{{tabs_index}}">
+            {content_{{tabs_index}}:content}
+        </div>
+    {{/tabs_each}}
+    </div>
 
 </div>
--- files/js/element.js
+++ files/js/element.js
@@ -2,42 +2,42 @@
  * This is required for element rendering to be possible
  * @type {PlatformElement}
  */
-(function(){
-	var TabbedBox = PlatformElement.extend({
-		events: {
-			'click .tabbed-box-tab': 'clickTab'
-		},
+(function() {
+    var TabbedBox = PlatformElement.extend({
+        events: {
+            'click .tabbed-box-tab': 'clickTab'
+        },
 
-		initialize: function() {
-			var tabs = this.$('.tabbed-box-tab');
-			var content = this.$('.tabbed-box-content');
+        initialize: function() {
+            var tabs = this.$('.tabbed-box-tab');
+            var content = this.$('.tabbed-box-content');
 
-			// Default to first tab
-			tabs.first().addClass('active');
-			content.first().show();
-		},
+            // Default to first tab
+            tabs.first().addClass('active');
+            content.first().show();
+        },
 
-		/*
-			Handle click event on a tab.
-			Uses 'rel' attribute of each tab as an index to access
-			the class of the corresponding content area.
-		*/
-		clickTab: function(e) {
-			var active = $(e.currentTarget); // Clicked tab
-			var content_wrapper = this.$('.tabbed-box-content-group'); // wrapper for all content divs
+        /*
+            Handle click event on a tab.
+            Uses 'rel' attribute of each tab as an index to access
+            the class of the corresponding content area.
+        */
+        clickTab: function(e) {
+            var active = $(e.currentTarget); // Clicked tab
+            var content_wrapper = this.$('.tabbed-box-content-group'); // wrapper for all content divs
 
-			// Set new active tab
-			active.siblings().removeClass('active');
-			active.addClass('active');
+            // Set new active tab
+            active.siblings().removeClass('active');
+            active.addClass('active');
 
 
-			// Set new active content
-			content_wrapper.children().hide();
-			content_wrapper.find('.' + active.attr('rel')).fadeIn();
+            // Set new active content
+            content_wrapper.children().hide();
+            content_wrapper.find('.' + active.attr('rel')).fadeIn();
 
-			// Stop propagation in case this is a nested tab app
-			e.stopPropagation();
-		}
-	});
-	return TabbedBox;
+            // Stop propagation in case this is a nested tab app
+            e.stopPropagation();
+        }
+    });
+    return TabbedBox;
 })();
