3ad7af3d209277b4546dab5aae78d7d0b593153d
puddi <puddizzle@gmail.com>
2015-10-06 17:57:58 -0700

    Going back to scrolling.

--- files/html/element.tpl
+++ files/html/element.tpl
@@ -1,17 +1,20 @@
 <div class="tabbed-box {{theme}}">
-    <div class="scrollArrow scrollArrow-left">
-        <span class="scrollArrow-text">&lt;</span>
+    <div class="tabbed-box-bar">
+        <ul class="tabbed-box-tab-group">
+        {{#tabs_each}}
+            <li class="tabbed-box-tab" rel="tab-{{tabs_index}}">
+                {header_{{tabs_index}}:text default="Headline {{tabs_index}}"}
+            </li>
+        {{/tabs_each}}
+        </ul>
+
+        <div class="scrollArrow scrollArrow-left">
+            <span class="scrollArrow-text">&lt;</span>
+        </div>
+        <div class="scrollArrow scrollArrow-right">
+            <span class="scrollArrow-text">&gt;</span>
+        </div>
     </div>
-    <div class="scrollArrow scrollArrow-right">
-        <span class="scrollArrow-text">&gt;</span>
-    </div>
-    <ul class="tabbed-box-tab-group">
-    {{#tabs_each}}
-        <li class="tabbed-box-tab" rel="tab-{{tabs_index}}">
-            {header_{{tabs_index}}:text default="Headline {{tabs_index}}"}
-        </li>
-    {{/tabs_each}}
-    </ul>
 
     <div class="tabbed-box-content-group">
     {{#tabs_each}}
--- files/js/element.js
+++ files/js/element.js
@@ -8,8 +8,10 @@
             'click .tabbed-box-tab': 'clickTab',
             // in case they type and that causes an overflow, we should determine scrollability here
             'keyup .tabbed-box-tab .paragraph': 'determineScrollability',
-            'click .scrollArrow-left': 'moveLeft',
-            'click .scrollArrow-right': 'moveRight'
+            'mousedown .scrollArrow-left': 'scrollLeft',
+            'mousedown .scrollArrow-right': 'scrollRight',
+            'touchstart .scrollArrow-left': 'scrollLeft',
+            'touchstart .scrollArrow-right': 'scrollRight'
         },
 
         initialize: function() {
@@ -27,6 +29,8 @@
             // we set it here as a property of the window.
             $(window).resize(function() {
                 this.determineScrollability();
+            }.bind(this)).on('mouseup touchend', function() {
+                this.stopScrolling();
             }.bind(this));
 
             // since scroll events don't propagate up, we have to bind it here as opposed to in the events object
@@ -37,11 +41,12 @@
             // determine whether or not we should show the scroll handlers
             this.determineScrollability();
 
-            // load first tab
+            // load whichever tab was open
             $(document).ready(function() {
                 this.scrollTabsBar.children().first().click();
             }.bind(this));
         },
+
 
         /*
             Handle click event on a tab.
@@ -52,7 +57,8 @@
             var active = $(e.currentTarget); // Clicked tab
             var content_wrapper = this.$('.tabbed-box-content-group'); // wrapper for all content divs
 
-            this.determineScroll(active);
+            this.stopScrolling();
+            this.determineClickScroll(active);
 
             // Set new active tab
             active.siblings().removeClass('active');
@@ -87,14 +93,14 @@
             var target = this.scrollTabsBar[0];
 
             // left handler
-            if (this.settings.get('activeTabIndexInternal') != 0) {
+            if (target.scrollLeft != 0) {
                 this.scrollArrowLeft.addClass('active');
             } else {
                 this.scrollArrowLeft.removeClass('active');
             }
 
             // right handler
-            if (this.settings.get('activeTabIndexInternal') != this.scrollTabsBar.children().length - 1) {
+            if (target.scrollLeft + target.clientWidth != target.scrollWidth) {
                 this.scrollArrowRight.addClass('active');
             } else {
                 this.scrollArrowRight.removeClass('active');
@@ -102,32 +108,57 @@
         },
 
         // scrolls the tabs bar to the left.
-        moveLeft: function(e) {
+        scrollLeft: function(e) {
             e.stopPropagation();
-            if (this.scrollArrowLeft.hasClass('active')) {
-                $(this.scrollTabsBar.children()[this.settings.get('activeTabIndexInternal') - 1]).click();
-            }
+            this.stopScrolling();
+            var scrollEl = this.scrollTabsBar[0];
+            var view = this;
+            var factor = 2;
+            this.scrollArrowRight.addClass('active');
+            this.scrollInterval = setInterval(function() {
+                scrollEl.scrollLeft -= Math.floor(factor);
+                factor *= 1.05;
+                if (scrollEl.scrollLeft == 0) {
+                    view.stopScrolling();
+                }
+            }, 10);
+
         },
 
         // scrolls the tabs bar to the right.
-        moveRight: function(e) {
+        scrollRight: function(e) {
             e.stopPropagation();
-            if (this.scrollArrowRight.hasClass('active')) {
-                $(this.scrollTabsBar.children()[this.settings.get('activeTabIndexInternal') + 1]).click();
-           }
+            this.stopScrolling();
+            var scrollEl = this.scrollTabsBar[0];
+            var view = this;
+            var factor = 2;
+            this.scrollArrowLeft.addClass('active');
+            this.scrollInterval = setInterval(function() {
+                scrollEl.scrollLeft += Math.floor(factor);
+                factor *= 1.05;
+                // forcefully stop the interval if it's not doing anything anymore
+                if (scrollEl.scrollLeft + scrollEl.clientWidth == scrollEl.scrollWidth) {
+                    view.stopScrolling();
+                }
+            }, 10);
         },
 
-        determineScroll: function(active) {
+        stopScrolling: function() {
+            clearInterval(this.scrollInterval); 
+            this.determineHandlers();
+        },
+
+        determineClickScroll: function(active) {
             // figure out where the element is, and if we need to change our view to show it
             var scroll;
 
             // determine where the element we're going to is in respect to the tabs bar
             // 25px is the size of the scroll arrows
             var leftSidePosition = active.position().left - 25;
-            var rightSidePosition = active.position().left + active.width() - 25;
+            var rightSidePosition = active.position().left + active.width() + 25;
             // the gap is how much space we want there to be between the selected element
             // when it's out of view, and we go to it.
-            var gap = 100; 
+            var gap = 50; 
             if (this.scrollTabsBar.children().index(active) == 0) {
                 // this the first element, so define the scroll to be 0.
                 scroll = 0;
@@ -147,23 +178,27 @@
             // start scrolling
             scroll = Math.floor(scroll);
             var delta = scroll - this.scrollTabsBar[0].scrollLeft;
+            var startedDown = false;
             if (delta != 0) {
                 clearInterval(this.scrollInterval);
                 var distance = 0;
                 var direction = (delta > 0 ? 1 : -1);
                 var speed = 1;
+                var factor = 1.25;  // for parabolic acceleration
                 this.scrollInterval = setInterval(function() {
                     if (distance >= Math.abs(delta)) {
-                        clearInterval(this.scrollInterval);
-                        return;
+                        this.stopScrolling();
                     }
-                    this.scrollTabsBar[0].scrollLeft += (Math.max(1, speed) * direction);
-                    distance += Math.max(1, speed);
-                    if (distance * 2 < Math.abs(delta)) {
-                        speed *= 1.25; // 5/4
-                    } else {
-                        speed *= 0.8; // 4/5
+                    this.scrollTabsBar[0].scrollLeft += (Math.max(1, Math.floor(speed)) * direction);
+                    distance += Math.max(1, Math.floor(speed));
+                    if (distance * 2 >= Math.abs(delta)) {  // if we've traversed more than half
+                        if (factor == 1.25) {   // 5/4
+                            factor = 1;         // 4/4
+                        } else {
+                            factor = 0.8;       // 4/5
+                        }
                     }
+                    speed *= factor;
                 }.bind(this), 20);
             }
         }
